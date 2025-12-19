038b4992ef7facb4431c58f3f17275a43b73daf3
link-short <dora.family228@gmail.com>
2025-12-19 16:10:25 +0500

    Update index.js

--- Manage/index.js
+++ Manage/index.js
@@ -6,8 +6,27 @@ if (chat_id)
 {
   if (telegram_reg_id) 
   {
-    alert("WinniBotFather.TelegramChatId('" + chat_id + "'); " + "WinniBotFather.TelegramRegId('" + telegram_reg_id + "'); ")
+    // alert("WinniBotFather.TelegramChatId('" + chat_id + "'); " + "WinniBotFather.TelegramRegId('" + telegram_reg_id + "'); ")
+    loadAndInsertContent('https://winnigames2024-original.github.io/mods/WinniFunctions/pc.html', 'content-container');
   }
+}
+
+
+
+
+
+async function loadAndInsertContent(url, targetDivId) {
+  try {
+    const response = await fetch(url);
+    const htmlContent = await response.text(); // Получаем HTML как текст
+
+    const targetDiv = document.getElementById(targetDivId);
+
+    targetDiv.insertAdjacentHTML('beforeend', htmlContent);
+
+    } catch (error) {
+      console.error('Ошибка при загрузке или вставке контента:', error);
+    }
 }
 
 
@@ -36,12 +55,12 @@ if (chat_id)
         function acceptUserCookies() {
             setCookie('appCookiesAccepted', 'true', 1); // Set cookie for 30 days
             document.getElementById('cookieConsent').style.display = 'none';
+          document.getElementById('content-container').style.display = 'block';
         }
 
         function rejectUserCookies() {
             alert("You have rejected cookies. App features have been disabled, restarting the app...");
             document.getElementById('cookieConsent').style.display = 'none';
-            alert("Restarting the app...")
             location.href = location.href
         }
 
@@ -49,5 +68,6 @@ if (chat_id)
         window.onload = function() {
             if (!getCookie('appCookiesAccepted')) {
                 document.getElementById('cookieConsent').style.display = 'block';
+                document.getElementById('content-container').style.display = 'none';
             }
         };
