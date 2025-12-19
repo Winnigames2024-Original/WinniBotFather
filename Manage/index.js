ef56ad172e03a85f56af2b563a9d92ea0695e8f8
link-short <dora.family228@gmail.com>
2025-12-19 15:26:05 +0500

    Create index.js

---
+++ Manage/index.js
@@ -0,0 +1,54 @@
+const urlParsdams = new URLSearchParams(window.location.search);
+const chat_id = urlParsdams.get('chat_id');
+const telegram_reg_id = urlParsdams.get('telegram_reg_id');
+
+if (chat_id) 
+{
+  if (telegram_reg_id) 
+  {
+    alert(chat_id + telegram_reg_id)
+    alert(chat_id)
+    alert(telegram_reg_id)
+  }
+}
+
+
+// JavaScript for cookie management
+        function setCookie(name, value, days) {
+            let expires = "";
+            if (days) {
+                let date = new Date();
+                date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+                expires = "; expires=" + date.toUTCString();
+            }
+            document.cookie = name + "=" + (value || "") + expires + "; path=/";
+        }
+
+        function getCookie(name) {
+            let nameEQ = name + "=";
+            let ca = document.cookie.split(';');
+            for (let i = 0; i < ca.length; i++) {
+                let c = ca[i];
+                while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+                if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+            }
+            return null;
+        }
+
+        function acceptCookies() {
+            setCookie('cookieAccepted', 'true', 30); // Set cookie for 30 days
+            document.getElementById('cookieConsent').style.display = 'none';
+        }
+
+        function rejectCookies() {
+            alert("You have rejected cookies. Some functionalities might be limited.");
+            document.getElementById('cookieConsent').style.display = 'none';
+            // You might implement further actions here, like limiting certain features
+        }
+
+        // Check if cookie consent has been given on page load
+        window.onload = function() {
+            if (!getCookie('cookieAccepted')) {
+                document.getElementById('cookieConsent').style.display = 'block';
+            }
+        };
